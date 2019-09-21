5f6b70a2418693ae261c230703669038085f91bb
Gediminas Krasauskas <gediminas.krasauskas@mif.stud.vu.lt>
2019-09-21 16:26:23 +0300

    Add Controller and Views for CRUD

---
+++ Data/SoftwareArchitectureContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoftwareArchitecture.Models
+{
+    public class SoftwareArchitectureContext : DbContext
+    {
+        public SoftwareArchitectureContext (DbContextOptions<SoftwareArchitectureContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SoftwareArchitecture.Models.Employee> Employee { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using SoftwareArchitecture.Models;
 
 namespace SoftwareArchitecture
 {
@@ -33,6 +35,9 @@ namespace SoftwareArchitecture
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<SoftwareArchitectureContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("SoftwareArchitectureContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
