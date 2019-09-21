a20b17b6ad0098733ce3c50310d2183b007343c9
Gediminas Krasauskas <gediminas.krasauskas@mif.stud.vu.lt>
2019-09-21 16:28:51 +0300

    Add an initial migration and Update the database

---
+++ Migrations/20190921132706_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SoftwareArchitecture.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PersonId = table.Column<string>(maxLength: 11, nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    EmployeeEmail = table.Column<string>(maxLength: 50, nullable: true),
+                    Salary = table.Column<int>(nullable: false),
+                    BirthDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+        }
+    }
+}
